a35946d3670c2a3481b08b7e2da8ca2ade933439
Oscar Ivan Gomez Zamudio <oigomezz@unal.edu.co>
2024-04-23 16:48:39 -0500

    Se configura las variables de entorno del proyecto

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const config = require("./config");
 
+const clientes = require("./modulos/clientes/rutas");
+
 const app = express();
 
 app.set("port", config.app.port);
+
+app.use("/api/clientes/", clientes);
 
 module.exports = app;
