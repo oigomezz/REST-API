9cb474cddc418fe154fa50fab688a16d7fc63533
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-05-28 11:26:34 -0500

    Restructuring of the project for v.2

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   app: {
-    port: process.env.PORT || 5555,
+    port: process.env.PORT || 3000,
   },
   mysql: {
     host: process.env.MYSQL_HOST || "localhost",
--- src/modulos/clientes/index.js
+++
@@ -1,4 +0,0 @@
-const db = require("../../db/mysql");
-const ctrl = require("./controlador");
-
-module.exports = ctrl(db);
