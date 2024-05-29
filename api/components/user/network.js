01e0417322bcc83e4b6cf27c91a5fc1494f65760
Oscar Ivan Gomez <oigomezz@unal.edu.co>
2024-05-29 17:15:40 -0500

    Refactor auth/network.js, add login function, update user/network.js, and add post/network.js to api/index.js

--- api/components/user/network.js
+++ api/components/user/network.js
@@ -12,7 +12,7 @@ router.get("/:id", get);
 router.post("/", upsert);
 router.put("/", secure("update"), upsert);
 router.post("/follow/:id", secure("follow"), follow);
-router.get('/:id/following', following);
+router.get("/:id/following", following);
 
 // Internal functions
 function list(req, res, next) {
@@ -43,6 +43,14 @@ function follow(req, res, next) {
   Controller.follow(req.user.id, req.params.id)
     .then((data) => {
       response.success(req, res, data, 201);
+    })
+    .catch(next);
+}
+
+function following(req, res, next) {
+  return Controller.following(req.params.id)
+    .then((data) => {
+      return response.success(req, res, data, 200);
     })
     .catch(next);
 }
